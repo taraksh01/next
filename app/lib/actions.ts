8f2d5b8b3f43319f364e06fcab30afa0d52b1a94
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-03-01 19:32:41 +0530

    Write method to delete invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -45,3 +45,9 @@ export async function updateInvoice(id: string, formData: FormData) {
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
+
+export async function deleteInvoice(id: string) {
+  sql`DELETE FROM invoices WHERE id=${id}`;
+
+  revalidatePath('/dashboard/invoices');
+}
